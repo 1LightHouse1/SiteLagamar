cc09312c923e9d667b5232dd928121ca0a6c01f4
LugaRamos <88544672+LugaRamos@users.noreply.github.com>
2023-10-03 17:16:14 -0300

    Seleção caminhos finalizados

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,12 +3,14 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+    // Fecha todas as seções abertas
+    var panels = document.getElementsByClassName("panel");
+    for (var j = 0; j < panels.length; j++) {
+      panels[j].style.maxHeight = null;
     }
+
+    // Abre ou fecha a seção clicada
+    var panel = this.nextElementSibling;
+    panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
   });
 }
