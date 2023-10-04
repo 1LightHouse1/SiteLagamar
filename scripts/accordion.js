f04c918d64f8e1887d5b158e018a5815f94e15c6
douglasrmachado <douglasroqmachado@gmail.com>
2023-10-04 16:04:54 -0300

    Realizadas alterações no css do accordion

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,11 +3,14 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    
     this.classList.toggle("active-accordion");
+    desselecionarAccordion();
     // Fecha todas as seções abertas
     var panels = document.getElementsByClassName("panel");
     for (var j = 0; j < panels.length; j++) {
       panels[j].style.maxHeight = null;
+
     }
 
     // Abre ou fecha a seção clicada
@@ -15,4 +18,12 @@ for (i = 0; i < acc.length; i++) {
     
     panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
   });
+}
+
+function desselecionarAccordion(){
+  for (var index = 0; index < acc.length; index++) {
+    if (acc[i].classList.value.match("active-accordion")) {
+      acc[i].classList.toggle("active-accordion");
+    }
+  }
 }
