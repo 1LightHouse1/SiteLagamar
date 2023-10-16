8dde3578c700b090766ed44668c5c8be81247d77
LeandroClayton <leandroclaytonp@gmail.com>
2023-10-16 07:07:37 -0300

    Dividi a funcionalidade para uma única função

--- scripts/accordion2.js
+++ scripts/accordion2.js
@@ -14,33 +14,38 @@ botao.forEach((element)=>{
             element.classList.toggle('active');
         } else element.classList.toggle('active');
         
-        
-        switch (element.id) {
-            case "sociodiversidade":
-                // Tentando fazer com que uma feche a outra
-                if (secaoBio.style.height != '0px') {
-                    secaoBio.style.height = 0+'px';
-                }
-                if(element.classList.contains('active')){
-                    secaoSocio.style.height = secaoBio.scrollHeight+'px';
-                }else{
-                    secaoSocio.style.height = 0+'px';
-                }
-                break;
-            case "biodiversidade":
-                if (secaoSocio.style.height != '0px') {
-                    secaoSocio.style.height = 0+'px';
-                }
-
-                if(element.classList.contains('active')){
-                    secaoBio.style.height = secaoBio.scrollHeight+'px';
-                }else{
-                    secaoBio.style.height = 0+'px';
-                }
-            break;
-            default:
-                break;
-        }
+        abreAcordeao(element);
 
     })
 })
+
+function abreAcordeao(elemento){
+    console.log('DEU BOA VIADO');
+    switch (elemento.id) {
+        case "sociodiversidade":
+            // Tentando fazer com que uma feche a outra
+            if (secaoBio.style.height != '0px') {
+                secaoBio.style.height = 0+'px';
+            }
+            if(elemento.classList.contains('active')){
+                secaoSocio.style.height = secaoBio.scrollHeight+'px';
+            }else{
+                secaoSocio.style.height = 0+'px';
+            }
+            break;
+        case "biodiversidade":
+            if (secaoSocio.style.height != '0px') {
+                secaoSocio.style.height = 0+'px';
+            }
+
+            if(elemento.classList.contains('active')){
+                secaoBio.style.height = secaoBio.scrollHeight+'px';
+            }else{
+                secaoBio.style.height = 0+'px';
+            }
+        break;
+        default:
+            break;
+    }
+
+}
