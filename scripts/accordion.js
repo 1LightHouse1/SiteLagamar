5cee8885a0cfe35a1d5b9d073a6a3235c666953f
Reydner Vinicius <vinicius.nas777@gmail.com>
2023-10-24 16:54:41 -0300

    alternando a visibilidade ao clicar no botão

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,9 +1,22 @@
+var section1Visible = false;
+var section2Visible = false;
+
 document.getElementById("biodiversidade").addEventListener("click", function () {
-    document.getElementById("section1").style.display = "block";
-    document.getElementById("section2").style.display = "none";
+    if (section1Visible) {
+        document.getElementById("section1").style.display = "none";
+        section1Visible = false;
+    } else {
+        document.getElementById("section1").style.display = "block";
+        section1Visible = true;
+      }
 });
 
 document.getElementById("sociodiversidade").addEventListener("click", function () {
-    document.getElementById("section1").style.display = "none";
-    document.getElementById("section2").style.display = "block";
+    if (section2Visible) {
+        document.getElementById("section2").style.display = "none";
+        section2Visible = false;
+    } else {
+        document.getElementById("section2").style.display = "block";
+        section2Visible = true;
+      }
 });
