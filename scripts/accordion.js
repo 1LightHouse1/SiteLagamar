530e49638deecc9aa4044309e219d1cd8b8461a8
douglasrmachado <douglasroqmachado@gmail.com>
2023-10-25 18:17:18 -0300

    Accordion Estilizadinho

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,20 +3,44 @@ var section2Visible = false;
 
 document.getElementById("biodiversidade").addEventListener("click", function () {
     if (section1Visible) {
-        document.getElementById("section1").style.display = "none";
+        var section1 =document.getElementById("section1");
+        section1.classList.toggle(".active");
+        section1.style.maxHeight = 0 + "px";
+        setTimeout(() =>{
+            section1.style.display = "none";
+        }, 2000)
         section1Visible = false;
+
     } else {
-        document.getElementById("section1").style.display = "block";
+        var section1 = document.getElementById("section1");
+        section1.classList.toggle(".active");
+        section1.style.display = "block";
+        section1.style.maxHeight = section1.scrollHeight+"px";
         section1Visible = true;
+        
       }
 });
 
 document.getElementById("sociodiversidade").addEventListener("click", function () {
     if (section2Visible) {
-        document.getElementById("section2").style.display = "none";
+        var section2 = document.getElementById("section2");
+        section2.classList.toggle(".active");
+        section2.style.maxHeight = 0+"px";
+        setTimeout(() =>{
+            section2.style.display = "none";
+        }, 2000)
         section2Visible = false;
     } else {
-        document.getElementById("section2").style.display = "block";
+        var section2 = document.getElementById("section2");
+        section2.classList.toggle(".active");
+        section2.style.display = "block";
+        setTimeout(() =>{
+            section2.style.maxHeight = section2.scrollHeight+"px";
+        }, 100)
+
         section2Visible = true;
+        
+        
+
       }
 });
