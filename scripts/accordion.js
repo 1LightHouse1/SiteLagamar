ac1225237b1ac297858911067d77fa554f88abd7
LeandroClayton <leandroclaytonp@gmail.com>
2023-11-06 14:57:45 -0300

    Accordion relativamente feito

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,3 +1,5 @@
+
+
 var section1Visible = false;
 var section2Visible = false;
 
@@ -8,15 +10,21 @@ document.getElementById("biodiversidade").addEventListener("click", function () 
         section1.style.maxHeight = 0 + "px";
         setTimeout(() =>{
             section1.style.display = "none";
-        }, 2000)
+        }, 2000);
         section1Visible = false;
 
     } else {
-        var section1 = document.getElementById("section1");
-        section1.classList.toggle(".active");
-        section1.style.display = "block";
-        section1.style.maxHeight = section1.scrollHeight+"px";
-        section1Visible = true;
+        if (section2Visible) {
+            var section2 = document.getElementById("section2");
+            fechaSecao(section2);
+            setTimeout(() =>{
+                section1 = document.getElementById("section1");
+                abreSecao(section1);
+            },2000)
+        }else{
+            var section1 = document.getElementById("section1");
+            abreSecao(section1);
+        }
         
       }
 });
@@ -28,16 +36,48 @@ document.getElementById("sociodiversidade").addEventListener("click", function (
         section2.style.maxHeight = 0+"px";
         setTimeout(() =>{
             section2.style.display = "none";
-        }, 2000)
+        }, 2000);
         section2Visible = false;
     } else {
-        var section2 = document.getElementById("section2");
-        section2.classList.toggle(".active");
-        section2.style.display = "block";
-        section2.style.maxHeight = section2.scrollHeight+"px";
-        section2Visible = true;
+        if (section1Visible) {
+            var section1 =document.getElementById("section1");
+            fechaSecao(section1);
+            setTimeout(() =>{
+                var section2 = document.getElementById("section2");
+                abreSecao(section2);
+            }, 2000);
+        }else{
+            var section2 = document.getElementById("section2");
+            abreSecao(section2);
+        }
         
         
 
     }
 });
+
+
+function fechaSecao(params) {
+ 
+    params.classList.toggle(".active");
+    params.style.maxHeight = 0+"px";
+    setTimeout(() =>{
+        params.style.display = "none";
+    }, 2000)
+    if (params.classList.contains("secao-biodiversidade")) {
+        section1Visible = false;  
+    } else{
+        section2Visible = false;
+    }
+}
+
+function abreSecao(params) {
+    params.classList.toggle(".active");
+    params.style.display = "block";
+    params.style.maxHeight = params.scrollHeight+"px";
+    if (params.classList.contains("secao-biodiversidade")) {
+        section1Visible = true;  
+    } else{
+        section2Visible = true;
+    }
+}
